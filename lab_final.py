377240482d2d6feff4391d6588f640eabd026086
SkarioT <vlad.skariot@gmail.com>
2020-04-23 12:28:56 +0300

    main_commit

---
+++ lab_final.py
@@ -0,0 +1,13 @@
+#Не знаю, как там в Лондоне, я не была. Может, там собака — друг человека. А у нас управдом — друг человека!
+source_string='Не знаю, как там в Лондоне, я не была. Может, там собака — друг человека. А у нас управдом — друг человека!'
+
+print('1)Колличество символов:\n',len(source_string))
+print('2)Revers string:\n',source_string[::-1])
+print('3)Каждое слово с большой буквы:\n',source_string.title())
+print('4)Весь текст просисными буквами:\n',source_string.upper())
+print('5)Число вхождений "нд", "ам", "о" в строку\n',source_string.find('нд'),source_string.find('ам'),source_string.find('о'))
+print('6)Заглане<->строчные:\n',source_string.swapcase())
+print('7)Исходная строка:\n',source_string)
+
+print("branch_1")
+#for hithab
